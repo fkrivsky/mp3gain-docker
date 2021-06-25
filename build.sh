0063b875b7c147fdd44f0014881f49a1e9310191
Filip Křivský <filip.krivsky@gmail.com>
2021-06-25 16:18:19 +0200

    Build & push script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+version=hirsute-$(date '+%Y-%m-%d')
+
+echo "Building docker image with tag $version..."
+docker build -t fkrivsky/mp3gain:"${version}" .
+docker tag fkrivsky/mp3gain:"${version}" fkrivsky/mp3gain:latest
+echo "Pushing tag '$version' to registry..."
+docker push fkrivsky/mp3gain:"${version}"
+echo "Pushing tag 'latest' to registry..."
+docker push fkrivsky/mp3gain:latest
