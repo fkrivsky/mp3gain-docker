366bad101f655ee5edddd6f468353c42ebdf1b49
Filip Křivský <filip.krivsky@gmail.com>
2021-08-06 22:39:21 +0200

    Fix line breaks.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-echo "mp3gain will run with the following parameters ${PARAMETERS} in $PARALLEL threads."
+echo "mp3gain will run with the parameters ${PARAMETERS} in $PARALLEL thread(s)."
 exec find /data -iname '*mp3' | parallel -j $PARALLEL mp3gain ${PARAMETERS} {}\;
